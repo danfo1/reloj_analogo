93a12692ab3529bfb903212247144ed9e37e11f9
danfo1 <139786109+danfo1@users.noreply.github.com>
2023-10-24 13:58:28 -0500

    Add files via upload

---
+++ reloj_analogo/script.js
@@ -0,0 +1,28 @@
+
+const deg = 6;
+
+const hora = document.querySelector('.hora');
+const minuto = document.querySelector('.minuto');
+const segundo = document.querySelector('.segundo');
+
+setInterval(() => {
+    let tiempo = new Date();
+    let hr = tiempo.getHours() * 30;
+    let min = tiempo.getMinutes() * deg;
+    let seg = tiempo.getSeconds() * deg;
+
+
+    hora.style.transform = `rotate(${(hr + min / 12)}deg)`;
+    minuto.style.transform = `rotate(${min}deg)`;
+    segundo.style.transform = `rotate(${seg}deg)`;
+}, 1000);
+
+
+
+
+
+
+
+
+
+
